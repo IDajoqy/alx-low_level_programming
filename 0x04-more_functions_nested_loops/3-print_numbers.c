3deb366151fa0feed5f5b0162f5a38140460ad3c
IDajoqy <idayatjimoh1998@gmail.com>
2023-09-14 09:04:33 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,12 @@
 /**
  * print_numbers - prints the numbers from 0 to 9 followed by a new line
  */
-void print_numbers(void)
-{
-       char digit;
-       for (digit = '0'; digit <= '9'; digit++)
-       {
+        void print_numbers(void)
+        {
+        char digit;
+        for (digit = '0'; digit <= '9'; digit++)
+        {
         _putchar(digit);
-       }
-       _putchar('\n');
+         }
+        _putchar('\n');
 }
