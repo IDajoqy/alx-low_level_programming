9ceb55a9da38027775f47393d385046ed2db184d
IDajoqy <idayatjimoh1998@gmail.com>
2023-09-14 09:21:45 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,12 @@
 /**
  * print_most_numbers - prints the numbers from 0 to 9 (excluding 2 and 4) followed by a new line
  */
-   void print_most_numbers(void)
-   {
-    char digit;
-
-    for (digit = '0'; digit <= '9'; digit++)
-    {                                                                    if (digit != '2' && digit != '4')
-    {							                _putchar(digit);
-   {				   		        			        }
-		    _putchar('\n');
+       void print_most_numbers(void)
+       {
+       char digit;
+       for (digit = '0'; digit <= '9'; digit++)
+       {                                                                    if (digit != '2' && digit != '4')
+       {							                _putchar(digit);
+       {	                	        			        }
+        _putchar('\n');
 }
