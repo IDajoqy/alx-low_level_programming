cc1a585d8d2a36e122136fb977d4520821f5040f
IDajoqy <idayatjimoh1998@gmail.com>
2023-12-08 20:04:42 -0800

    going back to c programming is nice

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print all the element of a 
+ * print_dlistint - prints all the elements of a
  * dlistint_t list
  *
  * @h: head of the list
  * Return: the number of nodes
  */
-size_t print_dlistint (const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
 
@@ -22,9 +22,9 @@ size_t print_dlistint (const dlistint_t *h)
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-	        count++;
-                h = h->next;
-         }
-         
-         return (count);
-}	 
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of element in
+ * dlistint_len - returns the number of elements in
  * a double linked list
  *
  * @h: head of the list
  * Return: the number of nodes
  */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
 
@@ -21,10 +21,9 @@ size_t dlistint_len(const dlistint_t *h);
 
 	while (h != NULL)
 	{
-	        count++;
+		count++;
 		h = h->next;
 	}
 
-	return(count);
+	return (count);
 }
- 
